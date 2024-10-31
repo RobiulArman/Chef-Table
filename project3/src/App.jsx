7659d033118d5acd55eb5f8cb01ef33cef762476
RobiulArman <robiularman5@gmail.com>
2024-11-01 05:07:24 +0600

    first commit

---
+++ project3/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Receipe from './Receipe'
+
+function App() {
+ 
+
+  return (
+    <>
+     
+      <h1 className='text-5xl'>Vite + React</h1>
+      <Receipe></Receipe>
+      
+    </>
+  )
+}
+
+export default App
---
+++ project3/src/Receipe.jsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+const Receipe = () => {
+    return (
+        <div className="text-center my-8 w-8/12 mx-auto space-y-4">
+            <h1 className="font-semibold text-5xl text-gray-800">Our Receips</h1>
+            <p className="text-base text-gray-600"></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quia voluptatibus maxime explicabo.
+                 Perspiciatis repellat commodi totam nam non magni, possimus quae amet ipsa fuga necessitatibus! Temporibus ullam quod cumque?</p>
+        </div>
+    );
+};
+
+export default Receipe;
